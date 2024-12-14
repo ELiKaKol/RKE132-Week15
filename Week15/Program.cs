a4f0b502d243842bbbb0369e7217a3e06215ffba
EliKaKol <elisak.kolga@gmail.com>
2024-12-14 15:11:19 +0200

    add week15 code

---
+++ Week15/Program.cs
@@ -0,0 +1,68 @@
+﻿List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+    if (tempArray.Length == 3)
+    {
+        string title = tempArray[0];
+        string rating = tempArray[1];
+        string year = tempArray[2];
+        Movie newMovie = new Movie(title, rating, year);
+        myMovies.Add(newMovie);
+    }
+    else
+    {
+        Console.WriteLine($"Warning: Skipping invalid line: {line}");
+    }
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was released in {movie.Year} with a rating of {movie.Rating}.");
+}
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"C:\Users\flami\OneDrive\Desktop\prog\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title;
+    string rating;
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Rating
+    {
+        get { return rating; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie(string _title, string _rating, string _year)
+    {
+        title = _title;
+        rating = _rating;
+        year = _year;
+    }
+}
